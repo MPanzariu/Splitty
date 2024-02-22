8a42cec6a9b270e9fabb2b53ea23cf2cdd4a52ff
Panzariu Matei-Alexandru <M.A.Panzariu@student.tudelft.nl>
2024-02-22 22:48:33 +0100

    Added methods for getting the total expenses and the list of expenses

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
@@ -14,5 +16,16 @@ public class EventController {
     Expense expense) {
         expenseService.addExpenseToEvent(eventId, expense);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @GetMapping("/{eventId}/expenses")
+    public ResponseEntity<List<Expense>> getAllExpensesForEvent(@PathVariable Long eventId) {
+        List<Expense> expenses = expenseService.getAllExpensesForEvent(eventId);
+        return ResponseEntity.ok(expenses);
+    }
+
+    @GetMapping("/{eventId}/total-expenses")
+    public ResponseEntity<Double> calculateTotalExpensesForEvent(@PathVariable Long eventId) {
+        double totalExpenses = expenseService.calculateTotalExpensesForEvent(eventId);
+        return ResponseEntity.ok(totalExpenses);
     }
 }
