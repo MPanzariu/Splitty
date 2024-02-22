393f8ccd441d7a7f4ac6eeaa83d881e4e09da02a
Giorgos Christofi <G.Christofi-1@student.tudelft.nl>
2024-02-22 12:56:15 +0100

    Added empty controller classes

---
+++ server/src/main/java/server/api/ExpenseController.java
@@ -0,0 +1,8 @@
+package server.api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+    //Empty expense controller class
+}
