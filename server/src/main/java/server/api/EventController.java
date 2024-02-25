3d45aed49df5db71fe30734689523ffaba95ae88
Glafkos Nikolas Michaelides <G.N.Michaelides@student.tudelft.nl>
2024-02-25 17:43:38 +0100

    Added the title editing and participant adding methods in the EventService and EventController classes. Not sure whether the title method needs a separate controller since it does not use the participant repository and hence would be better suited by a new constructor.

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -1,11 +1,9 @@
 package server.api;
+import commons.Event;
 import org.springframework.beans.factory.annotation.Autowired;
-import commons.Expense;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
@@ -20,5 +18,32 @@ public class EventController {
     public EventController(EventService eventService) {
         this.eventService = eventService;
     }
+
+    /**
+     * Updates the title of the event
+     * @param eventId identifies the event for which we want to update the title
+     * @param newTitle the new title
+     * @return returns "ok" if the operation succeeds
+     */
+    @PutMapping("/events/{eventId}")
+    public ResponseEntity<Event> editTitle(@PathVariable String eventId,
+                                           @RequestBody String newTitle){
+        Event updatedEvent = eventService.editTitle(eventId, newTitle);
+        return ResponseEntity.ok(updatedEvent);
+    }
+
+    /**
+     * Creates a new instance of a participant, which is then tied to an event
+     * @param eventId identifies the id for which we want to add a participant
+     * @param participantName the name of the participant we will add
+     * @return returns the status of the operation
+     */
+    @PostMapping("/{eventId}")
+    public ResponseEntity<Void> addParticipantToEvent(@PathVariable String eventId, @RequestBody
+    String participantName) {
+        eventService.addParticipantToEvent(participantName, eventId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 
 }
