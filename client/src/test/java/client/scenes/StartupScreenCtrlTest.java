0e204830c687f73545e961ad40b1614be41024d0
Giorgos Christofi <G.Christofi-1@student.tudelft.nl>
2024-02-26 22:56:16 +0100

    Debugging #3

--- client/src/test/java/client/scenes/StartupScreenCtrlTest.java
+++ client/src/test/java/client/scenes/StartupScreenCtrlTest.java
@@ -19,36 +19,36 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(ApplicationExtension.class)
 public class StartupScreenCtrlTest{
-//
-//    private StartupScreenCtrl sut;
-//    private TestServerUtils testServerUtils;
-//    private TestMainController testMainController;
-//    @Mock
-//    private TextField eventTitleTextBox;
-//
-//    @Mock
-//    private TextField inviteCodeTextBox;
-//
-//    @Mock
-//    private Label createEventFeedback;
-//
-//    @Mock
-//    private Label joinEventFeedback;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        this.testServerUtils = new TestServerUtils();
-//        this.testMainController =  new TestMainController();
-//        sut = new StartupScreenCtrl(this.testServerUtils, this.testMainController);
-//        eventTitleTextBox = new TextField();
-//        inviteCodeTextBox = new TextField();
-//        sut.setEventTitleTextBox(eventTitleTextBox);
-//        sut.setInviteCodeTextBox(inviteCodeTextBox);
-//        sut.setCreateEventFeedback(createEventFeedback);
-//        sut.setJoinEventFeedback(joinEventFeedback);
-//    }
-//
+
+    private StartupScreenCtrl sut;
+    private TestServerUtils testServerUtils;
+    private TestMainController testMainController;
+    @Mock
+    private TextField eventTitleTextBox;
+
+    @Mock
+    private TextField inviteCodeTextBox;
+
+    @Mock
+    private Label createEventFeedback;
+
+    @Mock
+    private Label joinEventFeedback;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        this.testServerUtils = new TestServerUtils();
+        this.testMainController =  new TestMainController();
+        sut = new StartupScreenCtrl(this.testServerUtils, this.testMainController);
+        eventTitleTextBox = new TextField();
+        inviteCodeTextBox = new TextField();
+        sut.setEventTitleTextBox(eventTitleTextBox);
+        sut.setInviteCodeTextBox(inviteCodeTextBox);
+        sut.setCreateEventFeedback(createEventFeedback);
+        sut.setJoinEventFeedback(joinEventFeedback);
+    }
+
 //    @Test
 //    public void testCreateEventEmptyTitle(){
 //        String title = "";
@@ -93,36 +93,36 @@ public class StartupScreenCtrlTest{
 //        assertEquals(1, testServerUtils.calls.size());
 //        assertEquals(1, testMainController.calls.size());
 //    }
-//
-//
-//
-//    public class TestServerUtils extends ServerUtils{
-//        public List<String> calls = new LinkedList<>();
-//        @Override
-//        public Event getEvent(String inviteCode){
-//            calls.add("getEvent: " + inviteCode);
-//            //valid code
-//            if(inviteCode.equals("aaaaaa")){
-//                return new Event();
-//            }
-//            //invalid code
-//            throw new jakarta.ws.rs.BadRequestException();
-//        }
-//
-//        @Override
-//        public Event createEvent(String inviteCode){
-//            calls.add("createEvent: " + inviteCode);
-//            return new Event();
-//        }
-//    }
-//
-//    public class TestMainController extends MainCtrl{
-//        public List<String> calls = new LinkedList<>();
-//        public Event lastEvent;
-//        @Override
-//        public void joinEvent(Event event){
-//            lastEvent = event;
-//            calls.add("join");
-//        }
-//    }
+
+
+
+    public class TestServerUtils extends ServerUtils{
+        public List<String> calls = new LinkedList<>();
+        @Override
+        public Event getEvent(String inviteCode){
+            calls.add("getEvent: " + inviteCode);
+            //valid code
+            if(inviteCode.equals("aaaaaa")){
+                return new Event();
+            }
+            //invalid code
+            throw new jakarta.ws.rs.BadRequestException();
+        }
+
+        @Override
+        public Event createEvent(String inviteCode){
+            calls.add("createEvent: " + inviteCode);
+            return new Event();
+        }
+    }
+
+    public class TestMainController extends MainCtrl{
+        public List<String> calls = new LinkedList<>();
+        public Event lastEvent;
+        @Override
+        public void joinEvent(Event event){
+            lastEvent = event;
+            calls.add("join");
+        }
+    }
 }
