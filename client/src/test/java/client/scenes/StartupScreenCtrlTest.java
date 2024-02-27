c009f4f18327018059f886d8c79ff834e474559d
Giorgos Christofi <G.Christofi-1@student.tudelft.nl>
2024-02-27 08:49:29 +0100

    Changed tests so they hopefully pass pipeline

--- client/src/test/java/client/scenes/StartupScreenCtrlTest.java
+++ client/src/test/java/client/scenes/StartupScreenCtrlTest.java
@@ -7,15 +7,12 @@ import javafx.scene.control.TextField;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.testfx.framework.junit5.ApplicationExtension;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
 public class StartupScreenCtrlTest{
@@ -23,80 +20,77 @@ public class StartupScreenCtrlTest{
     private StartupScreenCtrl sut;
     private TestServerUtils testServerUtils;
     private TestMainController testMainController;
-    @Mock
     private TextField eventTitleTextBox;
 
-    @Mock
     private TextField inviteCodeTextBox;
-
-    @Mock
     private Label createEventFeedback;
-
-    @Mock
     private Label joinEventFeedback;
 
     @BeforeEach
     public void setup() {
-        MockitoAnnotations.openMocks(this);
         this.testServerUtils = new TestServerUtils();
         this.testMainController =  new TestMainController();
         sut = new StartupScreenCtrl(this.testServerUtils, this.testMainController);
         eventTitleTextBox = new TextField();
         inviteCodeTextBox = new TextField();
+        createEventFeedback = new Label();
+        joinEventFeedback = new Label();
         sut.setEventTitleTextBox(eventTitleTextBox);
         sut.setInviteCodeTextBox(inviteCodeTextBox);
         sut.setCreateEventFeedback(createEventFeedback);
         sut.setJoinEventFeedback(joinEventFeedback);
     }
 
-//    @Test
+    @Test
     public void testCreateEventEmptyTitle(){
-//        String title = "";
-//        eventTitleTextBox.setText(title);
-//        sut.createEvent();
-//        assertTrue(testServerUtils.calls.isEmpty());
-//        assertTrue(testMainController.calls.isEmpty());
+        String title = "";
+        eventTitleTextBox.setText(title);
+        sut.createEvent();
+        assertTrue(testServerUtils.calls.isEmpty());
+        assertTrue(testMainController.calls.isEmpty());
+        assertEquals("Please specify the title!",createEventFeedback.getText());
     }
-//
-//    @Test
-//    public void testCreateEventSuccess(){
-//        String title = "title";
-//        eventTitleTextBox.setText(title);
-//        sut.createEvent();
-//        assertEquals(1, testServerUtils.calls.size());
-//        assertEquals(1, testMainController.calls.size());
-//    }
-//
-//    @Test
-//    public void testJoinEventInvalidLength(){
-//        String inviteCode = "invalid";
-//        inviteCodeTextBox.setText(inviteCode);
-//        sut.joinEvent();
-//        assertEquals(0, testServerUtils.calls.size());
-//        assertEquals(0, testMainController.calls.size());
-//    }
-//
-//    @Test
-//    public void testJoinEventInvalidCode(){
-//        String inviteCode = "aaaaab";
-//        inviteCodeTextBox.setText(inviteCode);
-//        sut.joinEvent();
-//        assertEquals(1, testServerUtils.calls.size());
-//        assertEquals(0, testMainController.calls.size());
-//    }
-//
-//    @Test
-//    public void testJoinEventValidCode(){
-//        String inviteCode = "aaaaaa";
-//        inviteCodeTextBox.setText(inviteCode);
-//        sut.joinEvent();
-//        assertEquals(1, testServerUtils.calls.size());
-//        assertEquals(1, testMainController.calls.size());
-//    }
 
+    @Test
+    public void testCreateEventSuccess(){
+        String title = "title";
+        eventTitleTextBox.setText(title);
+        sut.createEvent();
+        assertEquals(1, testServerUtils.calls.size());
+        assertEquals(1, testMainController.calls.size());
+        assertNotEquals("Please specify the title!",createEventFeedback.getText());
+    }
 
+    @Test
+    public void testJoinEventInvalidLength(){
+        String inviteCode = "invalid";
+        inviteCodeTextBox.setText(inviteCode);
+        sut.joinEvent();
+        assertEquals(0, testServerUtils.calls.size());
+        assertEquals(0, testMainController.calls.size());
+        assertEquals("Invalid invitation code!",joinEventFeedback.getText());
+    }
 
-    public class TestServerUtils extends ServerUtils{
+    @Test
+    public void testJoinEventInvalidCode(){
+        String inviteCode = "aaaaab";
+        inviteCodeTextBox.setText(inviteCode);
+        sut.joinEvent();
+        assertEquals(1, testServerUtils.calls.size());
+        assertEquals(0, testMainController.calls.size());
+        assertEquals("Invalid invitation code!",joinEventFeedback.getText());
+    }
+
+    @Test
+    public void testJoinEventValidCode(){
+        String inviteCode = "aaaaaa";
+        inviteCodeTextBox.setText(inviteCode);
+        sut.joinEvent();
+        assertEquals(1, testServerUtils.calls.size());
+        assertEquals(1, testMainController.calls.size());
+    }
+
+    private class TestServerUtils extends ServerUtils{
         public List<String> calls = new LinkedList<>();
         @Override
         public Event getEvent(String inviteCode){
@@ -116,7 +110,7 @@ public class StartupScreenCtrlTest{
         }
     }
 
-    public class TestMainController extends MainCtrl{
+    private class TestMainController extends MainCtrl{
         public List<String> calls = new LinkedList<>();
         public Event lastEvent;
         @Override
