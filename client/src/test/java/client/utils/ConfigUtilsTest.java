30ecb4eac3f6f2be4dd309d57a22881a34766bf2
Kornel Łuszczyk <k.luszczyk@student.tudelft.nl>
2024-02-28 16:56:32 +0100

    Added tests for ConfigUtils

---
+++ client/src/test/java/client/utils/ConfigUtilsTest.java
@@ -0,0 +1,65 @@
+package client.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigUtilsTest {
+    Properties testProperties;
+    Properties expectedProperties;
+    String versionString;
+    @BeforeEach
+    void setUp() {
+        testProperties = new Properties();
+        expectedProperties = new Properties();
+        expectedProperties.put("config.version", String.valueOf(ConfigUtils.CONFIG_VERSION));
+        versionString = "config.version=".concat(String.valueOf(ConfigUtils.CONFIG_VERSION));
+    }
+
+    /***
+     * Correct loading from Reader
+     */
+    @Test
+    void loadValidPropertiesFromReader() {
+        expectedProperties.put("key2", "value2");
+        Reader validReader = new StringReader(versionString.concat("\nkey2=value2"));
+        boolean success = ConfigUtils.loadValidPropertiesFromReader(testProperties, validReader);
+        assertTrue(success);
+        assertEquals(expectedProperties, testProperties);
+    }
+
+    /***
+     * Loading failure from out-of-date config
+     */
+    @Test
+    void loadOutdatedPropertiesFromReader() {
+        Reader outOfDateReader = new StringReader("no=versioning");
+        boolean success = ConfigUtils.loadValidPropertiesFromReader(testProperties, outOfDateReader);
+        assertFalse(success);
+    }
+
+    /***
+     * Comprehensive config loading test - by the nature of config loading this is dependent on the file system working
+     */
+    @Test
+    void easyLoadProperties() {
+        Properties returned = ConfigUtils.easyLoadProperties();
+        assertNotNull(returned);
+        assertNotEquals(0, returned.size());
+    }
+
+    /***
+     * Ensures the getDefault() method returns a usable Properties object
+     */
+    @Test
+    void getDefaultNotEmpty() {
+        Properties returned = ConfigUtils.getDefault();
+        assertNotNull(returned);
+        assertNotEquals(0, returned.size());
+    }
+}
