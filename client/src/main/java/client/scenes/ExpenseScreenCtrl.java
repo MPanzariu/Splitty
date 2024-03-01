681f64c67caf622ab55910c8332dd8be678fe215
Panzariu Matei-Alexandru <M.A.Panzariu@student.tudelft.nl>
2024-03-01 20:19:20 +0100

    Created ExpenseScreenCtrl class

--- client/src/main/java/client/scenes/ExpenseScreenCtrl.java
+++ client/src/main/java/client/scenes/ExpenseScreenCtrl.java
@@ -1,4 +1,47 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
+
 public class ExpenseScreenCtrl {
+    @FXML
+    private Label addEditExpense;
+    @FXML
+    private Label paidBy;
+    @FXML
+    private ComboBox<String> choosePayer;
+    @FXML
+    private Label purpose;
+    @FXML
+    private TextField expensePurpose;
+    @FXML
+    private Label amount;
+    @FXML
+    private TextField sum;
+    @FXML
+    private ComboBox<String> currency;
+    @FXML
+    private Label date;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Label splitMethod;
+    @FXML
+    private Label expenseType;
+    @FXML
+    private Checkbox splitBetweenAll;
+    @FXML
+    private Checkbox splitBetweenCustom;
+    @FXML
+    private Button cancel;
+    @FXML
+    private Button confirm;
+
 }
