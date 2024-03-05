6dcbefa902672e7adb05caf7916c4c2d1e7e8186
Iustin Nicolae Tudor <I.N.Tudor@student.tudelft.nl>
2024-03-05 15:17:39 +0100

    Random password generator done, can be seen in the console after starting up the server

---
+++ server/src/main/java/server/api/AdminPasswordService.java
@@ -0,0 +1,34 @@
+package server.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Service
+public class AdminPasswordService {
+    static final char[] passwordChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789".toCharArray();
+    static final int passwordLength = 20;
+    String password = "";
+    public String generatePassword() {
+        Random random = new Random();
+        for (int i = 0; i < passwordLength; i++) {
+            password += passwordChars[random.nextInt(passwordChars.length)];
+        }
+        return password;
+    }
+    private static final Logger log = LoggerFactory.getLogger(AdminPasswordService.class);
+    @Bean
+    CommandLineRunner initPassword() {
+        return args -> {
+            this.password = generatePassword();
+            log.info(password);
+        };
+    }
+}
