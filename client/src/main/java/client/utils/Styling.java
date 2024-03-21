4a41da9e530e79f845fdb705723191cc247e1878
Giorgos Christofi <G.Christofi-1@student.tudelft.nl>
2024-03-21 15:29:04 +0100

    Made styling method in Styling class more generic

--- client/src/main/java/client/utils/Styling.java
+++ client/src/main/java/client/utils/Styling.java
@@ -1,27 +1,28 @@
 package client.utils;
 
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 
 public class Styling {
     /**
-     * Adds the success styling to a label
+     * Adds the success styling to a node
      */
-    public static void addErrorStyling(Label label) {
-        if (label != null) {
-            label.getStyleClass().remove("successText");
-            if (!label.getStyleClass().contains("errorText")) {
-                label.getStyleClass().add("errorText");
+    public static void addErrorStyling(Node node) {
+        if (node != null) {
+            node.getStyleClass().remove("successText");
+            if (!node.getStyleClass().contains("errorText")) {
+                node.getStyleClass().add("errorText");
             }
         }
     }
     /**
-     * Adds the error styling to a label
+     * Adds the error styling to a node
      */
-    public static void addSuccessStyling(Label label) {
-        if (label != null) {
-            label.getStyleClass().remove("errorText");
-            if (!label.getStyleClass().contains("successText")) {
-                label.getStyleClass().add("successText");
+    public static void addSuccessStyling(Node node) {
+        if (node != null) {
+            node.getStyleClass().remove("errorText");
+            if (!node.getStyleClass().contains("successText")) {
+                node.getStyleClass().add("successText");
             }
         }
     }
